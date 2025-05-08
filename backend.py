07aac374d6c08c15b37fb01aa15af842a8567374
HenriquLopes <henriquelopesdebarros12345@gmail.com>
2025-05-08 14:23:20 -0300

    Adicionei acesso ao diretorio e link do stack

--- backend.py
+++ backend.py
@@ -1,10 +1,14 @@
+'''https://stackoverflow.com/questions/39066998/what-are-the-meaning-of-values-at-proc-pid-stat'''
+
 import os
 import classes 
 
 def pegaProcessos(self) -> list[classes.Processo]:
     
-    for pasta in os.scandir("/proc"):
-        pid = pasta.name
-        print(pid)
-    
+    for pid in os.scandir("/proc/"):
+        if(pid.name.isdigit()): #Coleta ids que números
+            pasta = open("/proc/{pid}/stat") #Abre o diretório stat de cada processo
+            #pid = pasta.name
+            #print(type(pasta.read()))
+            print(type(pasta))
 pegaProcessos(None)
